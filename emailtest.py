917c632c5aa82212978f610f2bba1a7be392d571
yuhao tan <tanyuhao@hotmail.com>
2020-06-01 18:56:57 +0800

    working copy with basic requirements

--- emailtest.py
+++ emailtest.py
@@ -2,22 +2,49 @@ import smtplib, ssl
 
 # Import the email modules we'll need
 from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 port = 587 # For ssl
 smtp_server = "smtp.office365.com"
-password = input("Type your password and press enter:")
 sender_email = "tanyuhao@hotmail.com"
+password = input("Type your password and press enter:")
 receiver_email = input("Type the receiver_email:")
-message = """\
-Subject: Hi there
+subject = input("Type the subject of the email:")
+content = input("Type the content of the email:")
+Attachment = input("Type any attachment file name:")
 
-This message is sent from Python."""
-#try:
+#Attachment details
+filename = Attachment
+with open(filename,"rb") as attachment:
+    # The content type "application/octet-stream" means that a MIME attachment is a binary file
+    part = MIMEBase('application','octet-stream')
+    part.set_payload(attachment.read())
+
+#Encode to base64
+encoders.encode_base64(part)
+
+# Add header
+part.add_header("Content-Disposition",
+                f"attachment; filename = {filename}"
+)
+
+# Create the container email EmailMessage
+msg = MIMEMultipart()
+msg['Subject'] = subject
+msg['From'] = sender_email
+msg['To'] = receiver_email #separate by ','
+# Add body to email
+msg.attach(MIMEText(content, 'plain'))
+# Add attachment to your message and convert it to string
+msg.attach(part)
+
+
 with smtplib.SMTP(smtp_server,port,timeout=20) as server:
 # SMTP automatically tries ESMTP EHLO first when there are no previous EHLO or HELO command in the session.
     server.starttls()
     server.login("tanyuhao@hotmail.com", password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.send_message(msg)
     print('Sent email successfully')
-#except Exception as e:
-#    print(str(e))
---
+++ readme.md
@@ -0,0 +1,16 @@
+#Emailtest
+
+What is it
+A way for people (coders) to send an email through command line quickly
+It should be faster than opening up an email client and sending it
+This means it is not meant for official and important emails. It is meant for quick and dirty emails in the form of chats
+Content is the main priority
+What is it not
+It is not meant to have full functionality and to be beautiful
+
+What do we need?
+Saving your default sender_email
+Quick inputs for receiver_email
+Inputs for subject
+Inputs for body
+Inputs for attachment
