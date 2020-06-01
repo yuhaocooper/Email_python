e38ff11faa75ccd2405fb64afb33420d0851034f
yuhao tan <tanyuhao@hotmail.com>
2020-06-01 12:24:34 +0800

    start of message changes

--- emailtest.py
+++ emailtest.py
@@ -1,19 +1,22 @@
 import smtplib, ssl
 
+# Import the email modules we'll need
+from email.message import EmailMessage
+
 port = 587 # For ssl
 smtp_server = "smtp.office365.com"
 password = input("Type your password and press enter:")
 sender_email = "tanyuhao@hotmail.com"
-receiver_email = "tanyuhao@gmail.com"
+receiver_email = input("Type the receiver_email:")
 message = """\
 Subject: Hi there
 
 This message is sent from Python."""
 
 with smtplib.SMTP(smtp_server,port,timeout=20) as server:
-    server.ehlo()
+    # SMTP automatically tries ESMTP EHLO first when there are no previous EHLO or HELO command in the session.
     server.starttls()
     server.login("tanyuhao@hotmail.com", password)
     server.sendmail(sender_email, receiver_email, message)
-    server.quit()
     print('Sent email successfully')
+except Exception as e: print(e)
